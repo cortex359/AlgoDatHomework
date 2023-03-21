638ace28218d4d05d4e45676cfacf50b282b2aeb
Christian Rene Thelen <christian.thelen@rwth-aachen.de>
2023-03-21 10:20:49 +0100

    tests removed

--- h01/src/Brackets.java
+++ h01/src/Brackets.java
@@ -2,7 +2,16 @@ import java.util.ArrayDeque;
 import java.util.Set;
 
 /**
+ * Brackets stellt die Funktion isValid bereit.
+ * ==========================================
+ * Hausaufgabe 01: Brackets
+ * Algorithmen und Datenstrukturen, SoSe 2023
+ * Aufgaben vom 20.03.2023
+ * Abgabe der Lösungen am 27.03.2023
  *
+ * @author Samuel Thesing, samuel.thesing@rwth-aachen.de
+ * @author Christian Rene Thelen, christian.thelen@rwth-aachen.de
+ * @author Michael Conrads, michael.conrads@rwth-aachen.de
  */
 public class Brackets {
 
@@ -18,12 +27,16 @@ public class Brackets {
         return '-';
     }
 
+    /**
+     * isValid überprüft einen String, bestehend aus Klammern auf Gültigkeit.
+     * @param s Zu überprüfender Ausdruck.
+     * @return True, wenn valide, ansonsten false.
+     */
     public static boolean isValid(String s) {
         Set<Character> open = Set.of('(', '[', '{');
         Set<Character> close = Set.of(')', ']', '}');
 
         ArrayDeque<Character> stack = new ArrayDeque<>();
-
         for (var c :  s.toCharArray()) {
             if (open.contains(c)) {
                 stack.push(c);
@@ -33,24 +46,5 @@ public class Brackets {
             }
         }
         return (stack.isEmpty());
-    }
-
-    public static void main(String[] args) {
-        if (isValid("(([[]]))"))
-            System.out.printf("(([[]]))     true\n");
-        if (isValid("([][](()()))"))
-            System.out.printf("([][](()())) true\n");
-        if (isValid("([)]"))
-            System.out.printf("([)]     false");
-        if (isValid("([]])"))
-            System.out.printf("([]])    false");
-        if (isValid("(()))"))
-            System.out.printf("(()))    false");
-        if (isValid("(()"))
-            System.out.printf("(()      false");
-        if (isValid("({[])}"))
-            System.out.printf("({[])}   false");
-        if (isValid(""))
-            System.out.printf("Gar keine Klammern   true\n");
     }
 }
