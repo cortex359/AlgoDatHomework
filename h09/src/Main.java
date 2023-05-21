8017dd3e38f2a377786cbfc623e1d4095b3cc002
Mike <idknowmw@gmail.com>
2023-05-21 13:43:09 +0200

    only terminates when input is correct.

--- h09/src/Main.java
+++ h09/src/Main.java
@@ -6,9 +6,12 @@ public class Main {
     public static void main(String[] args){
         try{
             BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-            int articleWeight = Integer.parseInt(reader.readLine());
-            System.out.println("Gewicht des Artikels: " + articleWeight);
-            Solver.backtracking(articleWeight);
+            int articleWeight;
+            do {
+                System.out.println("Geben Sie ein Gewicht zwischen 0 und 32 ein:");
+                articleWeight = Integer.parseInt(reader.readLine());
+                System.out.println("Gewicht des Artikels: " + articleWeight);
+            } while(!Solver.backtracking(articleWeight));
         }
         catch (IOException e){
             e.printStackTrace();
--- h09/src/Solver.java
+++ h09/src/Solver.java
@@ -2,38 +2,38 @@ import java.util.*;
 
 public class Solver {
     
-    private static final int[] _weights = {1, 3, 8, 20};
+    private static final int[] weights = {1, 3, 8, 20};
     private static final Stack<Integer> usedWeights = new Stack<>();
-    
     private static final ArrayList<Integer> possibleSolution = new ArrayList<>();
     private static final ArrayList<ArrayList<Integer>> solutions = new ArrayList<>();
     
-    public static void backtracking(int input){
-        if(input > Arrays.stream(_weights).sum() || input < 0){
+    public static boolean backtracking(int input){
+        if(input > Arrays.stream(weights).sum() || input < 0){
             System.out.println("Keine Lösung möglich");
-            return;
+            return false;
         }
         
         backtracking(0, input, 0);
         printSolutions();
+        return true;
     }
     private static void backtracking(int weightIndex, int targetWeight, int currentScaleWeight) {
         if (Math.abs(currentScaleWeight) == targetWeight) {
             Solver.rememberSolution();
         }
         
-        if (weightIndex >= _weights.length) {
+        if (weightIndex >= weights.length) {
             return;
         }
         
         //Gewicht wird auf die linke Seite gelegt
-        usedWeights.push(_weights[weightIndex]);
-        backtracking(weightIndex+1, targetWeight, currentScaleWeight + _weights[weightIndex]);
+        usedWeights.push(weights[weightIndex]);
+        backtracking(weightIndex+1, targetWeight, currentScaleWeight + weights[weightIndex]);
         usedWeights.pop();
         
         //Gewicht wird auf die rechte Seite gelegt
-        usedWeights.push(-_weights[weightIndex]);
-        backtracking(weightIndex+1, targetWeight, currentScaleWeight - _weights[weightIndex]);
+        usedWeights.push(-weights[weightIndex]);
+        backtracking(weightIndex+1, targetWeight, currentScaleWeight - weights[weightIndex]);
         usedWeights.pop();
         
         //Gewicht wird ignoriert
