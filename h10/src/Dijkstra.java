6bc6d99b0be5f30ed30b8281b9b1bac886fe0a18
Rankail <thes.samue@gmail.com>
2023-05-27 22:35:35 +0200

    H10 implementation

---
+++ h10/src/Dijkstra.java
@@ -0,0 +1,94 @@
+import java.util.*;
+import java.util.stream.IntStream;
+
+public class Dijkstra {
+
+    private record Kante(int zielKnoten, int gewicht) {}
+
+    private static List<List<Kante>> getAdjacencyListFromEdgeList(int[] kantenListe) {
+        if (kantenListe.length < 1) {
+            throw new IllegalArgumentException("Edge list is empty");
+        }
+
+        List<List<Kante>> kanten = new ArrayList<>(kantenListe[0]);
+        for (int i = 0; i < kantenListe[0]+1; i++) {
+            kanten.add(new ArrayList<>());
+        }
+
+        int c = 1;
+        while (c+2 < kantenListe.length) {
+            int anfang = kantenListe[c];
+            int ziel = kantenListe[c+1];
+            int gewicht = kantenListe[c+2];
+            if (anfang > kanten.size() || anfang < 1 || ziel > kanten.size() || ziel < 1) {
+                throw new IllegalArgumentException("Node-Index out of bounds.");
+            }
+
+            kanten.get(anfang).add(new Kante(ziel, gewicht));
+            c += 3;
+        }
+
+        return kanten;
+    }
+
+    private static int getIndexOfMinimumNotVisited(int[] arr, boolean[] visited) {
+        int min = Integer.MAX_VALUE;
+        int idx = -1;
+        for (int i = 0; i < arr.length; i++) {
+            if (!visited[i] && arr[i] < min) {
+                min = arr[i];
+                idx = i;
+            }
+        }
+        return idx;
+    }
+
+    private static void printData(int[] data, int undefinedValue) {
+        System.out.print("|");
+        for (int i = 2; i < data.length; i++) {
+            if (data[i] == undefinedValue) {
+                System.out.printf(" %2s", "--");
+            } else {
+                System.out.printf(" %2d", data[i]);
+            }
+        }
+    }
+
+    public static void printDijkstra(int[] kantenListe) {
+        List<List<Kante>> kanten = getAdjacencyListFromEdgeList(kantenListe);
+
+        System.out.print("vi");
+        var indices = IntStream.range(0, kanten.size()).toArray();
+        printData(indices, -1);
+        printData(indices, -1);
+        System.out.println("|");
+        System.out.println(("-").repeat(5 + 6 * (kanten.size()-2)));
+
+        int[] parents = new int[kanten.size()];
+        int[] distances = new int[kanten.size()];
+        boolean[] visited = new boolean[kanten.size()];
+        Arrays.fill(distances, Integer.MAX_VALUE);
+
+        distances[1] = 0;
+        while (true) {
+            int newlyAdded = getIndexOfMinimumNotVisited(distances, visited);
+            if (newlyAdded < 1) break;
+
+            visited[newlyAdded] = true;
+
+            for (Kante kante : kanten.get(newlyAdded)) {
+                if (distances[newlyAdded] + kante.gewicht < distances[kante.zielKnoten]) {
+                    distances[kante.zielKnoten] = distances[newlyAdded] + kante.gewicht;
+                    parents[kante.zielKnoten] = newlyAdded;
+                }
+            }
+
+            System.out.printf("%2s", newlyAdded);
+            printData(distances, Integer.MAX_VALUE);
+            printData(parents, 0);
+            System.out.println("|");
+        }
+
+    }
+
+}
---
+++ h10/src/DijkstraTest.java
@@ -0,0 +1,23 @@
+public class DijkstraTest {
+
+    public static void main(String[] args) {
+        Dijkstra.printDijkstra(new int[]{4, 1,2,2, 1,4,5, 2,4,1, 2,3,4, 3,1,1, 4,3,1});
+        System.out.println();
+
+        Dijkstra.printDijkstra(new int[]{
+            10,1,2,30,1,3,10,2,5,15,2,8,55,3,4,5,3,9,35,4,2,10,4,5,45,4,6,10,5,3,20,5,7,15,5,9,25,6,7,5,7,10,20,8,10,15,
+            9,8,10,9,10,30
+        });
+        System.out.println();
+
+        Dijkstra.printDijkstra(new int[]{
+            6, 1,2,5, 1,3,20, 2,3,10, 2,4,50, 3,2,20, 3,5,20, 4,5,20, 4,6,20, 5,4,5, 5,6,30
+        });
+        System.out.println();
+
+        Dijkstra.printDijkstra(new int[]{
+            6, 1,2,20, 1,3,10, 2,4,30, 2,5,20, 3,4,10, 3,5,20, 4,6,10, 5,6,30
+        });
+    }
+
+}
