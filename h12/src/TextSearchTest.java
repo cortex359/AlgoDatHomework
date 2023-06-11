5d17e5971ca4184617a00ff472c8bffb67471a54
Mike <idknowmw@gmail.com>
2023-06-11 21:50:49 +0200

    h12 corrected

--- h12/src/TextSearchTest.java
+++ h12/src/TextSearchTest.java
@@ -1,8 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.regex.PatternSyntaxException;
 
 public class TextSearchTest {
     public static void main(String[] args){
-        System.out.println(TextSearch.textSearch("AJHGAKGGADGASAG", "GA"));
-        System.out.println(TextSearch.textSearch("agdhGHAJGVAGÖOLJR", "[aB].dh"));
-        System.out.println(TextSearch.textSearch("Hi", "[Hi"));
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String text = "";
+        String pattern = "";
+        System.out.println("Bitte gib den Text ein.");
+        try {
+            text = reader.readLine();
+            System.out.println("Bitte gib das Pattern ein.");
+            pattern = reader.readLine();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+        try {
+            System.out.println(TextSearch.textSearch(text, pattern));
+        } catch(PatternSyntaxException p){
+            p.printStackTrace();
+        }
     }
 }
